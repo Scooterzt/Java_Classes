9bad93f300b91d11d738530a481f14cb7683978f
Roman Roman <Scooterzt@gmail.com>
2018-05-09 02:25:57 -0700

    New IDEA project created

--- SandBox/src/main/java/MyFirstJava.java
+++ SandBox/src/main/java/MyFirstJava.java
@@ -2,6 +2,6 @@ public class MyFirstJava {
 
     public static void main(String[] args) {
 
-        System.out.println("My fisrt java test");
+        System.out.println("My first java test");
     }
 }
